6c5696c86d897ef8f477e976c861ee8c13ef3460
Josh Shemas <joshshemas@gmail.com>
2016-02-05 15:11:40 -0500

    linting codebase

--- app.js
+++ app.js
@@ -2,106 +2,126 @@ var request = require('request'),
 	cheerio = require('cheerio'),
 	_ = require('lodash');
 
-module.exports = function(options, callback){
-	exports.getInfo(options, function(err,results){
-		callback(err,results);
+module.exports = function (options, callback) {
+	exports.getInfo(options, function (err, results) {
+		callback(err, results);
 	});
 };
 
-var fieldsArray = [{
-    multiple: false,
-    property: 'og:title',
-    fieldName: 'ogTitle'
-}, {
-    multiple: false,
-    property: 'og:type',
-    fieldName: 'ogType'
-}, {
-    multiple: true,
-    property: 'og:image',
-    fieldName: 'ogImage'
-}, {
-    multiple: true,
-    property: 'og:image:width',
-    fieldName: 'ogImageWidth'
-}, {
-    multiple: true,
-    property: 'og:image:height',
-    fieldName: 'ogImageHeight'
-}, {
-    multiple: true,
-    property: 'og:image:type',
-    fieldName: 'ogImageType'
-}, {
-    multiple: false,
-    property: 'og:url',
-    fieldName: 'ogUrl'
-}, {
-    multiple: false,
-    property: 'og:audio',
-    fieldName: 'ogAudio'
-}, {
-    multiple: false,
-    property: 'og:description',
-    fieldName: 'ogDescription'
-}, {
-    multiple: false,
-    property: 'og:determiner',
-    fieldName: 'ogDeterminer'
-}, {
-    multiple: false,
-    property: 'og:locale',
-    fieldName: 'ogLocale'
-}, {
-    multiple: false,
-    property: 'og:locale:alternate',
-    fieldName: 'ogLocaleAlternate'
-}, {
-    multiple: false,
-    property: 'og:site_name',
-    fieldName: 'ogSiteName'
-}, {
-    multiple: true,
-    property: 'og:video',
-    fieldName: 'ogVideo'
-}, {
-    multiple: true,
-    property: 'og:video:width',
-    fieldName: 'ogVideoWidth'
-}, {
-    multiple: true,
-    property: 'og:video:height',
-    fieldName: 'ogVideoHeight'
-}, {
-    multiple: true,
-    property: 'og:video:type',
-    fieldName: 'ogVideoType'
-}];
+var fieldsArray = [
+	{
+		multiple: false,
+		property: 'og:title',
+		fieldName: 'ogTitle'
+	},
+	{
+		multiple: false,
+		property: 'og:type',
+		fieldName: 'ogType'
+	},
+	{
+		multiple: true,
+		property: 'og:image',
+		fieldName: 'ogImage'
+	},
+	{
+		multiple: true,
+		property: 'og:image:width',
+		fieldName: 'ogImageWidth'
+	},
+	{
+		multiple: true,
+		property: 'og:image:height',
+		fieldName: 'ogImageHeight'
+	},
+	{
+		multiple: true,
+		property: 'og:image:type',
+		fieldName: 'ogImageType'
+	},
+	{
+		multiple: false,
+		property: 'og:url',
+		fieldName: 'ogUrl'
+	},
+	{
+		multiple: false,
+		property: 'og:audio',
+		fieldName: 'ogAudio'
+	},
+	{
+		multiple: false,
+		property: 'og:description',
+		fieldName: 'ogDescription'
+	},
+	{
+		multiple: false,
+		property: 'og:determiner',
+		fieldName: 'ogDeterminer'
+	},
+	{
+		multiple: false,
+		property: 'og:locale',
+		fieldName: 'ogLocale'
+	},
+	{
+		multiple: false,
+		property: 'og:locale:alternate',
+		fieldName: 'ogLocaleAlternate'
+	},
+	{
+		multiple: false,
+		property: 'og:site_name',
+		fieldName: 'ogSiteName'
+	},
+	{
+		multiple: true,
+		property: 'og:video',
+		fieldName: 'ogVideo'
+	},
+	{
+		multiple: true,
+		property: 'og:video:width',
+		fieldName: 'ogVideoWidth'
+	},
+	{
+		multiple: true,
+		property: 'og:video:height',
+		fieldName: 'ogVideoHeight'
+	},
+	{
+		multiple: true,
+		property: 'og:video:type',
+		fieldName: 'ogVideoType'
+	}
+];
 
-var mediaMapper = function(item) {
-    return {
-        url: item[0],
-        width: item[1],
-        height: item[2],
-        type: item[3]
-    }
+var mediaMapper = function (item) {
+	return {
+		url: item[0],
+		width: item[1],
+		height: item[2],
+		type: item[3]
+	};
 };
 
-var mediaSorter = function(a, b) {
-    if (!(a.url && b.url))
-        return 0;
+var mediaSorter = function (a, b) {
+	if (!(a.url && b.url)) {
+		return 0;
+	}
 
-    var aRes = a.url.match(/\.(\w{2,5})$/),
-        aExt = (aRes && aRes[1].toLowerCase()) || null;
-    var bRes = b.url.match(/\.(\w{2,5})$/),
-        bExt = (bRes && bRes[1].toLowerCase()) || null;
+	var aRes = a.url.match(/\.(\w{2,5})$/),
+		aExt = (aRes && aRes[1].toLowerCase()) || null;
+	var bRes = b.url.match(/\.(\w{2,5})$/),
+		bExt = (bRes && bRes[1].toLowerCase()) || null;
 
-    if (aExt == 'gif' && bExt != 'gif')
-        return -1;
-    else if (aExt != 'gif' && bExt == 'gif')
-        return 1;
-    else
-        return Math.max(b.width, b.height) - Math.max(a.width, a.height);
+	if (aExt === 'gif' && bExt !== 'gif') {
+		return -1;
+	} else if (aExt !== 'gif' && bExt === 'gif') {
+		return 1;
+	} else {
+		return Math.max(b.width, b.height) - Math.max(a.width, a.height);
+	}
 };
 
 /*
@@ -109,48 +129,48 @@ var mediaSorter = function(a, b) {
  * @param string url - user input of url
  * @param function callback
  */
-exports.getInfo = function(options, callback){
-	var error = null, returnResule = {};
-	that = this;
-	this.validateVars(options.url, options.timeout, function(inputUrlFlag, inputUrl, inputTimeoutFlag, inputTimeout){
-		if(inputUrlFlag && inputUrlFlag == true && inputTimeoutFlag && inputTimeoutFlag == true){
+exports.getInfo = function (options, callback) {
+	var error = null, returnResult = {},
+		that = this;
+	this.validateVars(options.url, options.timeout, function (inputUrlFlag, inputUrl, inputTimeoutFlag, inputTimeout) {
+		if (inputUrlFlag && inputUrlFlag === true && inputTimeoutFlag && inputTimeoutFlag === true) {
 			options.url = inputUrl;
 			options.timeout = inputTimeout;
-			that.getOG(options, function(err, results) {
-				if(results && results.success){
-					returnResule = {
+			that.getOG(options, function (err, results) {
+				if (results && results.success) {
+					returnResult = {
 						data: results,
 						success: true
 					};
-				}else{
-					if(err && (err.code == 'ENOTFOUND' || err.code == 'EHOSTUNREACH')){
+				} else {
+					if (err && (err.code === 'ENOTFOUND' || err.code === 'EHOSTUNREACH')) {
 						error = 'err';
-						returnResule = {
+						returnResult = {
 							err: 'Page Not Found',
 							success: false
 						};
-					} else if(err && err.code == 'ETIMEDOUT'){
+					} else if (err && err.code === 'ETIMEDOUT') {
 						error = 'err';
-						returnResule = {
+						returnResult = {
 							err: 'Time Out',
 							success: false
 						};
-					}else{
+					} else {
 						error = 'err';
-						returnResule = {
+						returnResult = {
 							err: 'Page Not Found',
 							success: false
 						};
 					}
-				};
-				callback(error,returnResule);
+				}
+				callback(error, returnResult);
 			});
-		}else{
-			callback('err',{
+		} else {
+			callback('err', {
 				success: false,
 				err: 'Invalid URL'
 			});
-		};
+		}
 	});
 };
 
@@ -159,28 +179,31 @@ exports.getInfo = function(options, callback){
  * @param string var - user input
  * @param function callback
  */
-exports.validateVars = function(inputUrl, inputTimeout, callback) {
-	var returnInputUrl,returnInputUrlFlag,returnInputTimeout,returnInputTimeoutFlag;
-	if ( inputUrl == null || inputUrl.length < 1 || typeof inputUrl === 'undefined' || !inputUrl) {
+exports.validateVars = function (inputUrl, inputTimeout, callback) {
+	var returnInputUrl,
+		returnInputUrlFlag,
+		returnInputTimeout,
+		returnInputTimeoutFlag;
+	if (inputUrl === null || typeof inputUrl === 'undefined' || !inputUrl || inputUrl.length < 1) {
 		returnInputUrlFlag = false;
 		returnInputUrl = '';
 	} else {
 		returnInputUrlFlag = true;
-		returnInputUrl = this.validateUrl(inputUrl)
-	};
-	if ( inputTimeout == null || inputTimeout.length < 1 || typeof inputTimeout === 'undefined' || !inputTimeout || typeof inputTimeout !== 'number' ) {
+		returnInputUrl = this.validateUrl(inputUrl);
+	}
+	if (inputTimeout === null || typeof inputTimeout === 'undefined' || !inputTimeout || typeof inputTimeout !== 'number' || inputTimeout.length < 1) {
 		returnInputTimeoutFlag = true;
 		returnInputTimeout = 2000; //time default to 2000ms
 	} else {
-		if(this.validateTimeout(inputTimeout)){
+		if (this.validateTimeout(inputTimeout)) {
 			returnInputTimeoutFlag = true;
 			returnInputTimeout = inputTimeout;
 		} else {
 			returnInputTimeoutFlag = true;
 			returnInputTimeout = 2000; //time default to 2000ms
 		}
-	};
-	callback(returnInputUrlFlag, returnInputUrl, returnInputTimeoutFlag, returnInputTimeout)
+	}
+	callback(returnInputUrlFlag, returnInputUrl, returnInputTimeoutFlag, returnInputTimeout);
 };
 
 /*
@@ -188,10 +211,10 @@ exports.validateVars = function(inputUrl, inputTimeout, callback) {
  * @param string var - the url we want to scrape
  * @param function callback
  */
-exports.validateUrl = function(inputUrl) {
-	if(!/^(f|ht)tps?:\/\//i.test(inputUrl)) {
-		inputUrl = "http://" + inputUrl;
-	};
+exports.validateUrl = function (inputUrl) {
+	if (!/^(f|ht)tps?:\/\//i.test(inputUrl)) {
+		inputUrl = 'http://' + inputUrl;
+	}
 	return inputUrl;
 };
 
@@ -200,10 +223,10 @@ exports.validateUrl = function(inputUrl) {
  * @param number var - the time we want to wait
  * @param function callback
  */
-exports.validateTimeout = function(inputTimeout) {
-	if(!/^\d{1,10}$/.test(inputTimeout)) {
+exports.validateTimeout = function (inputTimeout) {
+	if (!/^\d{1,10}$/.test(inputTimeout)) {
 		return false;
-	};
+	}
 	return true;
 };
 
@@ -212,9 +235,9 @@ exports.validateTimeout = function(inputTimeout) {
  * @param string url - the url we want to scrape
  * @param function callback
  */
-exports.getOG = function(options, callback) {
-	request(options, function(err, response, body) {
-		if(err){
+exports.getOG = function (options, callback) {
+	request(options, function (err, response, body) {
+		if (err) {
 			callback(err, null);
 		} else {
 			var $ = cheerio.load(body),
@@ -225,22 +248,22 @@ exports.getOG = function(options, callback) {
 			//able to get og info
 			ogObject.success = 'true';
 
-			keys.forEach(function(key){
-				if(!(meta[key].attribs && meta[key].attribs.property))
+			keys.forEach(function (key) {
+				if (!(meta[key].attribs && meta[key].attribs.property)) {
 					return;
-
+				}
 				var property = meta[key].attribs.property,
-				content = meta[key].attribs.content;
-
-				fieldsArray.forEach(function(item){
-					if (property === item.property){
-						if(!item.multiple)
+					content = meta[key].attribs.content;
+				fieldsArray.forEach(function (item) {
+					if (property === item.property) {
+						if (!item.multiple) {
 							ogObject[item.fieldName] = content;
-						else if(!ogObject[item.fieldName])
+						} else if (!ogObject[item.fieldName]) {
 							ogObject[item.fieldName] = [content];
-						else if(Array.isArray(ogObject[item.fieldName]))
+						} else if (Array.isArray(ogObject[item.fieldName])) {
 							ogObject[item.fieldName].push(content);
-					};
+						}
+					}
 				});
 			});
 
@@ -261,26 +284,28 @@ exports.getOG = function(options, callback) {
 				.map(mediaMapper).sort(mediaSorter);
 
 			// Delete temporary fields
-			fieldsArray.filter(function(item){
+			fieldsArray.filter(function (item) {
 				return item.multiple;
-			}).forEach(function(item) {
+			}).forEach(function (item) {
 				delete ogObject[item.fieldName];
 			});
 
 			// Select the best image
-			if (ogImages.length)
+			if (ogImages.length) {
 				ogObject.ogImage = ogImages[0];
+			}
 
 			// Select the best video
-			if (ogVideos.length)
+			if (ogVideos.length) {
 				ogObject.ogVideo = ogVideos[0];
+			}
 
 			//example of how to get the title tag
 			// $('title').map(function(i, info) {
 			// 	console.log('title:',info.children[0].data);
 			// });
 			//console.log('ogObject',ogObject);
-			callback(null,ogObject);
-		};
+			callback(null, ogObject);
+		}
 	});
 };
--- tests/index.spec.js
+++ tests/index.spec.js
@@ -3,172 +3,172 @@ var app = require('../app'),
 
 //test url - this has alot of OG info
 var options1 = {
-	'url':'http://ogp.me/'
+	'url': 'http://ogp.me/'
 };
 
 //test url formats
 var	options2 = {
-		'url':'http://www.google.com/'
+		'url': 'http://www.google.com/'
 	},
 	options3 = {
-		'url':'https://www.google.com/'
+		'url': 'https://www.google.com/'
 	},
 	options4 = {
-		'url':'www.google.com/'
+		'url': 'www.google.com/'
 	},
 	options5 = {
-		'url':'google.com/'
+		'url': 'google.com/'
 	},
 	options6 = {
-		'url':'http://google.com/'
+		'url': 'http://google.com/'
 	};
 
 //invaild url
 var options7 = {
-	'url':'http://testtesttest4564568.com'
+	'url': 'http://testtesttest4564568.com'
 };
 
 //empty value
 var optionsEmpty = {
-	'url':''
+	'url': ''
 };
 
 // test timeout
 var options8 = {
-		'url':'http://www.google.com/',
-		'timeout':2000
+		'url': 'http://www.google.com/',
+		'timeout': 2000
 	},
 	options9 = {
-		'url':'http://www.google.com/',
-		'timeout':''
+		'url': 'http://www.google.com/',
+		'timeout': ''
 	},
 	options10 = {
-		'url':'http://www.google.com/',
-		'timeout':'2000'
+		'url': 'http://www.google.com/',
+		'timeout': '2000'
 	},
 	options11 = {
-		'url':'http://www.google.com/',
-		'timeout':'sdsdds'
+		'url': 'http://www.google.com/',
+		'timeout': 'sdsdds'
 	};
 
 // some bad urls
 var options12 = {
-		'url':23233
+		'url': 23233
 	},
 	options13 = {
-		'url':'2323233'
+		'url': '2323233'
 	},
 	options14 = {
-		'url':'this is a testt'
+		'url': 'this is a testt'
 	};
 
 //no url
 var optionsNoUrl = { };
 
-describe('GET OG', function (done) {
+describe('GET OG', function () {
 	this.timeout(3000); //shoudl wait atleast 3secs before failing
-	it('Valid call og - url1', function(done) {
-		app(options1, function(err, result){
+	it('Valid call og - url1', function (done) {
+		app(options1, function (err, result) {
 			expect(err).to.be(null);
 			expect(result.success).to.be(true);
 			done();
 		});
 	});
-	it('Valid call og - url2', function(done) {
-		app(options2, function(err, result){
+	it('Valid call og - url2', function (done) {
+		app(options2, function (err, result) {
 			expect(err).to.be(null);
 			expect(result.success).to.be(true);
 			done();
 		});
 	});
-	it('Valid call og - url3', function(done) {
-		app(options3, function(err, result){
+	it('Valid call og - url3', function (done) {
+		app(options3, function (err, result) {
 			expect(err).to.be(null);
 			expect(result.success).to.be(true);
 			done();
 		});
 	});
-	it('Valid call og - url4', function(done) {
-		app(options4, function(err, result){
+	it('Valid call og - url4', function (done) {
+		app(options4, function (err, result) {
 			expect(err).to.be(null);
 			expect(result.success).to.be(true);
 			done();
 		});
 	});
-	it('Valid call og - url5', function(done) {
-		app(options5, function(err, result){
+	it('Valid call og - url5', function (done) {
+		app(options5, function (err, result) {
 			expect(err).to.be(null);
 			expect(result.success).to.be(true);
 			done();
 		});
 	});
-	it('Valid call og - url6', function(done) {
-		app(options6, function(err, result){
+	it('Valid call og - url6', function (done) {
+		app(options6, function (err, result) {
 			expect(err).to.be(null);
 			expect(result.success).to.be(true);
 			done();
 		});
 	});
-	it('Invalid call og - url7', function(done) {
-		app(options7, function(err, result){
+	it('Invalid call og - url7', function (done) {
+		app(options7, function (err, result) {
 			expect(result.success).to.be(false);
 			done();
 		});
 	});
-	it('Invalid get og - empty url', function(done) {
-		app(optionsEmpty, function(err, result){
+	it('Invalid get og - empty url', function (done) {
+		app(optionsEmpty, function (err, result) {
 			expect(result.err).to.be('Invalid URL');
 			expect(result.success).to.be(false);
 			done();
 		});
 	});
-		it('Valid call og - url8', function(done) {
-		app(options8, function(err, result){
+	it('Valid call og - url8', function (done) {
+		app(options8, function (err, result) {
 			expect(result.success).to.be(true);
 			done();
 		});
 	});
-	it('Valid call og - url9', function(done) {
-		app(options9, function(err, result){
+	it('Valid call og - url9', function (done) {
+		app(options9, function (err, result) {
 			expect(result.success).to.be(true);
 			done();
 		});
 	});
-	it('Valid call og - url10', function(done) {
-		app(options10, function(err, result){
+	it('Valid call og - url10', function (done) {
+		app(options10, function (err, result) {
 			expect(result.success).to.be(true);
 			done();
 		});
 	});
-	it('Valid call og - url11', function(done) {
-		app(options11, function(err, result){
+	it('Valid call og - url11', function (done) {
+		app(options11, function (err, result) {
 			expect(result.success).to.be(true);
 			done();
 		});
 	});
-	it('Valid call og - url12', function(done) {
-		app(options12, function(err, result){
+	it('Valid call og - url12', function (done) {
+		app(options12, function (err, result) {
 			expect(result.success).to.be(false);
 			expect(result.err).to.be('Page Not Found');
 			done();
 		});
 	});
-	it('Valid call og - url13', function(done) {
-		app(options13, function(err, result){
+	it('Valid call og - url13', function (done) {
+		app(options13, function (err, result) {
 			expect(result.success).to.be(false);
 			expect(result.err).to.be('Page Not Found');
 			done();
 		});
 	});
-	it('Valid call og - url14', function(done) {
-		app(options14, function(err, result){
+	it('Valid call og - url14', function (done) {
+		app(options14, function (err, result) {
 			expect(result.success).to.be(false);
 			expect(result.err).to.be('Page Not Found');
 			done();
 		});
 	});
-	it('Invalid get og - no url', function(done) {
-		app(optionsNoUrl, function(err, result){
+	it('Invalid get og - no url', function (done) {
+		app(optionsNoUrl, function (err, result) {
 			expect(result.err).to.be('Invalid URL');
 			expect(result.success).to.be(false);
 			done();
