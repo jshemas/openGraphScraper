413c676c5311c3a04c35181949e3a1732c283ddd
jshemas <joshshemas@gmail.com>
2016-09-04 16:36:04 -0400

    fixing tests

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -279,7 +279,7 @@ describe('GET OG', function () {
 			expect(err).to.be(false);
 			expect(result.success).to.be(true);
 			expect(result.data.ogTitle).to.be('Twitter. It\'s what\'s happening.');
-			expect(result.data.ogDescription).to.be('Connect with your friends &#8212; and other fascinating people. Get in-the-moment updates on the things that interest you. And watch events unfold, in real time, from every angle.');
+			expect(result.data.ogDescription).to.be('From breaking news and entertainment to sports and politics, get the full story with all the live commentary.');
 			done();
 		});
 	});
