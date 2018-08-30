e839f4a79ef27d02695ac646089613102599cda2
jshemas <joshshemas@gmail.com>
2018-08-30 17:49:19 -0400

    fixing tests

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -573,7 +573,7 @@ describe('GET OG', function () {
       expect(error).to.be(false);
       expect(result.success).to.be(true);
       expect(result.requestUrl).to.be('https://www.namecheap.com/');
-      expect(result.data.ogTitle).to.be('\n\tDomain Name Registration - Buy Domain Names from $0.48 - Namecheap\n');
+      expect(result.data.ogTitle).to.be('\n\tDomain Names Starting at $0.88 - Namecheap.com\n');
       done();
     });
   });
