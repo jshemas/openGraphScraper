1ef259d0779cf21dc632561ff5dc6e0679531d3a
jshemas <joshshemas@gmail.com>
2020-05-26 21:50:38 -0400

    adding more openGraphScraper unit tests and fixing small bugs

--- tests/unit/openGraphScraper.spec.js
+++ tests/unit/openGraphScraper.spec.js
@@ -1,7 +1,29 @@
 const got = require('got');
 const openGraphScraper = require('../../lib/openGraphScraper');
 
-const basicHTML = '<html><head><title>test page</title></head><body><h1>hello test page</h2></body></html>';
+const basicHTML = `
+  <html>
+    <head>
+      <title>test page</title>
+      <meta charset="utf-8">
+      <meta property="og:description" content="test description">
+    </head>
+    <body>
+      <h1>hello test page</h1>
+      <img width="360" src="test.png" alt="test">
+    </body>
+  </html>`;
+
+const encodingHTML = `
+  <html>
+    <head>
+      <title>тестовая страница</title>
+      <meta property="og:description" content="привет тестовая страница<">
+    </head>
+    <body>
+      <h1>привет тестовая страница<</h1>
+    </body>
+  </html>`;
 
 const sandbox = sinon.createSandbox();
 
@@ -11,62 +33,178 @@ describe('openGraphScraper', function () {
   });
 
   context('should be able to hit site and find OG title info', function () {
-    beforeEach(async function () {
-      sandbox.stub(got, 'get').resolves({ body: basicHTML });
-    });
-    it('using callbacks', function (done) {
-      openGraphScraper({ url: 'www.test.com' }, function (error, result, response) {
-        expect(error).to.be.eql(false);
-        expect(result.success).to.be.eql(true);
-        expect(result.ogImage).to.be.eql([]);
-        expect(result.ogTitle).to.be.eql('test page');
-        expect(result.requestUrl).to.be.eql('http://www.test.com');
-        expect(response.body).to.be.eql(basicHTML);
-        done();
+    context('using just a url', function () {
+      beforeEach(async function () {
+        sandbox.stub(got, 'get').resolves({ body: basicHTML });
+      });
+      it('using callbacks', function (done) {
+        openGraphScraper({ url: 'www.test.com' }, function (error, result, response) {
+          expect(error).to.be.eql(false);
+          expect(result.success).to.be.eql(true);
+          expect(result.ogTitle).to.be.eql('test page');
+          expect(result.requestUrl).to.be.eql('http://www.test.com');
+          expect(response.body).to.be.eql(basicHTML);
+          done();
+        });
+      });
+      it('using promises', function () {
+        return openGraphScraper({ url: 'www.test.com' })
+          .then(function (data) {
+            expect(data.result.success).to.be.eql(true);
+            expect(data.result.ogTitle).to.be.eql('test page');
+            expect(data.result.requestUrl).to.be.eql('http://www.test.com');
+            expect(data.response.body).to.be.eql(basicHTML);
+          })
+          .catch(function () {
+            expect().fail('this should not happen');
+          });
       });
     });
-    it('using promises', function () {
-      return openGraphScraper({ url: 'www.test.com' })
-        .then(function (data) {
-          expect(data.result.success).to.be.eql(true);
-          expect(data.result.ogImage).to.be.eql([]);
-          expect(data.result.ogTitle).to.be.eql('test page');
-          expect(data.result.requestUrl).to.be.eql('http://www.test.com');
-          expect(data.response.body).to.be.eql(basicHTML);
-        })
-        .catch(function () {
-          expect().fail('this should not happen');
-        });
-    });
-  });
 
-  context('should be able hit when site is not on blacklist', function () {
-    beforeEach(async function () {
-      sandbox.stub(got, 'get').resolves({ body: basicHTML });
-    });
-    it('using callbacks', function (done) {
-      openGraphScraper({ url: 'www.test.com', blacklist: ['testtest.com'] }, function (error, result, response) {
-        expect(error).to.be.eql(false);
-        expect(result.success).to.be.eql(true);
-        expect(result.ogImage).to.be.eql([]);
-        expect(result.ogTitle).to.be.eql('test page');
-        expect(result.requestUrl).to.be.eql('http://www.test.com');
-        expect(response.body).to.be.eql(basicHTML);
-        done();
+    context('with html', function () {
+      beforeEach(async function () {
+        sandbox.stub(got, 'get').resolves({ body: basicHTML });
+      });
+      it('using callbacks', function (done) {
+        openGraphScraper({ html: basicHTML }, function (error, result, response) {
+          expect(error).to.be.eql(false);
+          expect(result.success).to.be.eql(true);
+          expect(result.ogTitle).to.be.eql('test page');
+          expect(result.requestUrl).to.be.eql(null);
+          expect(response.body).to.be.eql(basicHTML);
+          done();
+        });
+      });
+      it('using promises', function () {
+        return openGraphScraper({ html: basicHTML })
+          .then(function (data) {
+            expect(data.result.success).to.be.eql(true);
+            expect(data.result.ogTitle).to.be.eql('test page');
+            expect(data.result.requestUrl).to.be.eql(null);
+            expect(data.response.body).to.be.eql(basicHTML);
+          })
+          .catch(function () {
+            expect().fail('this should not happen');
+          });
       });
     });
-    it('using promises', function () {
-      return openGraphScraper({ url: 'www.test.com', blacklist: ['testtest.com'] })
-        .then(function (data) {
-          expect(data.result.success).to.be.eql(true);
-          expect(data.result.ogImage).to.be.eql([]);
-          expect(data.result.ogTitle).to.be.eql('test page');
-          expect(data.result.requestUrl).to.be.eql('http://www.test.com');
-          expect(data.response.body).to.be.eql(basicHTML);
-        })
-        .catch(function () {
-          expect().fail('this should not happen');
+
+    context('when site is not on blacklist', function () {
+      beforeEach(async function () {
+        sandbox.stub(got, 'get').resolves({ body: basicHTML });
+      });
+      it('using callbacks', function (done) {
+        openGraphScraper({ url: 'www.test.com', blacklist: ['testtest.com'] }, function (error, result, response) {
+          expect(error).to.be.eql(false);
+          expect(result.success).to.be.eql(true);
+          expect(result.ogTitle).to.be.eql('test page');
+          expect(result.requestUrl).to.be.eql('http://www.test.com');
+          expect(response.body).to.be.eql(basicHTML);
+          done();
         });
+      });
+      it('using promises', function () {
+        return openGraphScraper({ url: 'www.test.com', blacklist: ['testtest.com'] })
+          .then(function (data) {
+            expect(data.result.success).to.be.eql(true);
+            expect(data.result.ogTitle).to.be.eql('test page');
+            expect(data.result.requestUrl).to.be.eql('http://www.test.com');
+            expect(data.response.body).to.be.eql(basicHTML);
+          })
+          .catch(function () {
+            expect().fail('this should not happen');
+          });
+      });
+    });
+
+    context('with encoding set to null and withCharset set to true', function () {
+      beforeEach(async function () {
+        sandbox.stub(got, 'get').resolves({ body: Buffer.from(encodingHTML, 'utf8') });
+      });
+      it('using callbacks', function (done) {
+        openGraphScraper({ url: 'www.test.com', encoding: null, withCharset: true }, function (error, result, response) {
+          expect(error).to.be.eql(false);
+          expect(result.success).to.be.eql(true);
+          expect(result.charset).to.be.eql(null);
+          expect(result.ogTitle).to.be.eql('тестовая страница');
+          expect(result.ogDescription).to.be.eql('привет тестовая страница<');
+          expect(result.requestUrl).to.be.eql('http://www.test.com');
+          expect(response.body).to.be.eql(Buffer.from(encodingHTML, 'utf8'));
+          done();
+        });
+      });
+      it('using promises', function () {
+        return openGraphScraper({ url: 'www.test.com', encoding: null, withCharset: true })
+          .then(function (data) {
+            expect(data.result.success).to.be.eql(true);
+            expect(data.result.charset).to.be.eql(null);
+            expect(data.result.ogTitle).to.be.eql('тестовая страница');
+            expect(data.result.ogDescription).to.be.eql('привет тестовая страница<');
+            expect(data.result.requestUrl).to.be.eql('http://www.test.com');
+            expect(data.response.body).to.be.eql(Buffer.from(encodingHTML, 'utf8'));
+          })
+          .catch(function () {
+            expect().fail('this should not happen');
+          });
+      });
+    });
+
+    context('with withCharset set to true', function () {
+      beforeEach(async function () {
+        sandbox.stub(got, 'get').resolves({ body: basicHTML });
+      });
+      it('using callbacks', function (done) {
+        openGraphScraper({ url: 'www.test.com', withCharset: true }, function (error, result, response) {
+          expect(error).to.be.eql(false);
+          expect(result.success).to.be.eql(true);
+          expect(result.ogTitle).to.be.eql('test page');
+          expect(result.requestUrl).to.be.eql('http://www.test.com');
+          expect(response.body).to.be.eql(basicHTML);
+          done();
+        });
+      });
+      it('using promises', function () {
+        return openGraphScraper({ url: 'www.test.com', withCharset: true })
+          .then(function (data) {
+            expect(data.result.success).to.be.eql(true);
+            expect(data.result.ogTitle).to.be.eql('test page');
+            expect(data.result.requestUrl).to.be.eql('http://www.test.com');
+            expect(data.response.body).to.be.eql(basicHTML);
+          })
+          .catch(function () {
+            expect().fail('this should not happen');
+          });
+      });
+    });
+
+    context('as a browser', function () {
+      beforeEach(async function () {
+        sandbox.stub(got, 'get').resolves({ body: basicHTML });
+      });
+      it('using callbacks', function (done) {
+        process.browser = true;
+        openGraphScraper({ url: 'www.test.com' }, function (error, result, response) {
+          expect(error).to.be.eql(false);
+          expect(result.success).to.be.eql(true);
+          expect(result.ogTitle).to.be.eql('test page');
+          expect(result.requestUrl).to.be.eql('http://www.test.com');
+          expect(response.body).to.be.eql(basicHTML);
+          done();
+        });
+      });
+      it('using promises', function () {
+        process.browser = true;
+        return openGraphScraper({ url: 'www.test.com' })
+          .then(function (data) {
+            expect(data.result.success).to.be.eql(true);
+            expect(data.result.ogTitle).to.be.eql('test page');
+            expect(data.result.requestUrl).to.be.eql('http://www.test.com');
+            expect(data.response.body).to.be.eql(basicHTML);
+          })
+          .catch(function () {
+            expect().fail('this should not happen');
+          });
+      });
     });
   });
 
@@ -181,6 +319,34 @@ describe('openGraphScraper', function () {
             expect(data.result.error).to.eql('Invalid URL');
             expect(data.result.errorDetails.toString()).to.eql('Error: Invalid URL');
             expect(data.result.requestUrl).to.be.eql('');
+            expect(data.response).to.be.eql(undefined);
+          });
+      });
+    });
+
+    context('when trying to hit a URL and you are passing in a HTML page', function () {
+      it('using callbacks', function (done) {
+        openGraphScraper({ url: 'www.test.com', html: basicHTML }, function (error, result, response) {
+          expect(error).to.be.eql(true);
+          expect(result.success).to.be.eql(false);
+          expect(result.error).to.eql('Must specify either `url` or `html`, not both');
+          expect(result.errorDetails.toString()).to.eql('Error: Must specify either `url` or `html`, not both');
+          expect(result.requestUrl).to.be.eql('www.test.com');
+          expect(response).to.be.eql(undefined);
+          done();
+        });
+      });
+      it('using promises', function () {
+        return openGraphScraper({ url: 'www.test.com', html: basicHTML })
+          .then(function () {
+            expect().fail('this should not happen');
+          })
+          .catch(function (data) {
+            expect(data.error).to.be.eql(true);
+            expect(data.result.error).to.eql('Must specify either `url` or `html`, not both');
+            expect(data.result.errorDetails.toString()).to.eql('Error: Must specify either `url` or `html`, not both');
+            expect(data.result.requestUrl).to.eql('www.test.com');
+            expect(data.result.success).to.eql(false);
             expect(data.response).to.be.eql(undefined);
           });
       });
