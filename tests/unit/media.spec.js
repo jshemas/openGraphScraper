edf52fca516303c1c9771063fa4c83f25a3f7e5a
jshemas <joshshemas@gmail.com>
2020-05-26 20:02:19 -0400

    clean up media unit tests

--- tests/unit/media.spec.js
+++ tests/unit/media.spec.js
@@ -1,186 +1,231 @@
-/* eslint  no-undefined: 0 */
-
 const media = require('../../lib/media');
 
 describe('media', function () {
-  describe('media', function () {
-    it('media setup', function (done) {
-      const ogMedia = media.mediaSetup({
-        ogTitle: 'test site',
-        ogType: 'website',
-        ogUrl: 'http://test.com/',
-        ogImage: ['http://test.com/logo.png'],
-        ogImageType: ['image/png'],
-        ogImageWidth: ['300'],
-        ogImageHeight: ['300'],
-        twitterImage: ['http://test.com/logo.png'],
-        twitterImageAlt: ['image/png'],
-        twitterImageWidth: ['300'],
-        twitterImageHeight: ['300'],
-        ogDescription: 'stuff',
-      }, {
-        url: 'http://test.com/',
-      });
-      expect(ogMedia.ogImage).to.eql({
-        url: 'http://test.com/logo.png',
-        width: '300',
-        height: '300',
-        type: 'image/png',
-      });
-      expect(ogMedia.twitterImage).to.eql({
-        url: 'http://test.com/logo.png',
-        width: '300',
-        height: '300',
-        alt: 'image/png',
-      });
-      done();
+  it('has images and twitter images', function (done) {
+    const ogMedia = media.mediaSetup({
+      ogImage: ['http://test.com/logo.png'],
+      ogImageType: ['image/png'],
+      ogImageWidth: ['300'],
+      ogImageHeight: ['300'],
+      twitterImage: ['http://test.com/logo.png'],
+      twitterImageAlt: ['image/png'],
+      twitterImageWidth: ['300'],
+      twitterImageHeight: ['300'],
+    }, {});
+
+    expect(ogMedia.ogImage).to.eql({
+      url: 'http://test.com/logo.png',
+      width: '300',
+      height: '300',
+      type: 'image/png',
     });
-    it('media setup - no image or video', function (done) {
-      const ogMedia = media.mediaSetup({
-        ogTitle: 'test site',
-        ogType: 'website',
-        ogUrl: 'http://test.com/',
-        ogDescription: 'stuff',
-      }, {
-        url: 'http://test.com/',
-      });
-      expect(ogMedia.ogImage).to.eql(undefined);
-      expect(ogMedia.twitterImage).to.eql(undefined);
-      expect(ogMedia.ogVideo).to.eql(undefined);
-      expect(ogMedia.twitterPlayer).to.eql(undefined);
-      done();
+
+    expect(ogMedia.twitterImage).to.eql({
+      url: 'http://test.com/logo.png',
+      width: '300',
+      height: '300',
+      alt: 'image/png',
     });
-    it('media setup - has video', function (done) {
-      const ogMedia = media.mediaSetup({
-        ogTitle: 'test site',
-        ogType: 'website',
-        ogUrl: 'http://test.com/',
-        ogVideo: ['http://test.com/logo.png'],
-        ogVideoType: ['image/png'],
-        ogVideoWidth: ['300'],
-        ogVideoHeight: ['300'],
-        twitterPlayer: ['http://test.com/logo.png'],
-        twitterPlayerStream: ['image/png'],
-        twitterPlayerWidth: ['300'],
-        twitterPlayerHeight: ['300'],
-        ogDescription: 'stuff',
-      }, {
-        url: 'http://test.com/',
-      });
-      expect(ogMedia.ogVideo).to.eql({
-        url: 'http://test.com/logo.png',
-        width: '300',
-        height: '300',
-        type: 'image/png',
-      });
-      expect(ogMedia.twitterPlayer).to.eql({
-        url: 'http://test.com/logo.png',
-        width: '300',
-        height: '300',
-        stream: 'image/png',
-      });
-      done();
+
+    done();
+  });
+  it('has many images and twitter images', function (done) {
+    const ogMedia = media.mediaSetup({
+      ogImage: ['http://test.com/logo_one.png', 'http://test.com/logo_two.png', 'http://test.com/logo_two.png', ''],
+      ogImageType: ['image/png'],
+      ogImageWidth: ['300'],
+      ogImageHeight: ['300'],
+      twitterImage: ['http://test.com/logo_one.png', 'http://test.com/logo_two.png', 'http://test.com/logo_two.png', ''],
+      twitterImageAlt: ['image/png'],
+      twitterImageWidth: ['300'],
+      twitterImageHeight: ['300'],
+    }, {});
+
+    expect(ogMedia.ogImage).to.eql({
+      url: 'http://test.com/logo_one.png',
+      width: '300',
+      height: '300',
+      type: 'image/png',
     });
-    it('media setup - has music:song', function (done) {
-      const ogMedia = media.mediaSetup({
-        ogTitle: 'test site',
-        ogType: 'website',
-        ogUrl: 'http://test.com/',
-        musicSong: ['http://test.com/songurl'],
-        musicSongTrack: ['1'],
-        musicSongDisc: ['1'],
-        ogDescription: 'stuff',
-      }, {
-        url: 'http://test.com/',
-      });
-      expect(ogMedia.musicSong).to.eql({
-        url: 'http://test.com/songurl',
-        track: '1',
-        disc: '1',
-      });
-      done();
+
+    expect(ogMedia.twitterImage).to.eql({
+      url: 'http://test.com/logo_one.png',
+      width: '300',
+      height: '300',
+      alt: 'image/png',
     });
-    it('media setup - has multiple music:songs with allMedia set to true', function (done) {
-      const ogMedia = media.mediaSetup({
-        ogTitle: 'test site',
-        ogType: 'website',
-        ogUrl: 'http://test.com/',
-        musicSong: ['http://test.com/songurl', 'http://test.com/songurl3', 'http://test.com/songurl2'],
-        musicSongTrack: ['1', '2', '4'],
-        musicSongDisc: ['1', '2', '1'],
-        ogDescription: 'stuff',
-      }, {
-        url: 'http://test.com/',
-        allMedia: true,
-      });
-      expect(ogMedia.musicSong).to.eql([{
-        url: 'http://test.com/songurl',
-        track: '1',
-        disc: '1',
-      },
-      {
-        url: 'http://test.com/songurl2',
-        track: '4',
-        disc: '1',
-      },
-      {
-        url: 'http://test.com/songurl3',
-        track: '2',
-        disc: '2',
-      }]);
-      done();
+
+    done();
+  });
+  it('has a .gif images and twitter images', function (done) {
+    const ogMedia = media.mediaSetup({
+      ogImage: ['http://test.com/logo_one.png', 'http://test.com/logo_two.gif'],
+      ogImageType: ['image/png', 'image/gif'],
+      ogImageWidth: ['300', '600'],
+      ogImageHeight: ['300', '600'],
+      twitterImage: ['http://test.com/logo_two.gif', 'http://test.com/logo_one.png'],
+      twitterImageAlt: ['image/gif', 'image/png'],
+      twitterImageWidth: ['300', '600'],
+      twitterImageHeight: ['300', '600'],
+    }, {});
+
+    expect(ogMedia.ogImage).to.eql({
+      url: 'http://test.com/logo_two.gif',
+      type: 'image/gif',
+      width: '600',
+      height: '600',
     });
-    it('media setup - allMedia set to true', function (done) {
-      const ogMedia = media.mediaSetup({
-        ogTitle: 'test site',
-        ogType: 'website',
-        ogUrl: 'http://test.com/',
-        ogImage: ['http://test.com/logo.png'],
-        ogImageType: ['image/png'],
-        ogImageWidth: ['300'],
-        ogImageHeight: ['300'],
-        twitterImage: ['http://test.com/logo.png'],
-        twitterImageAlt: ['image/png'],
-        twitterImageWidth: ['300'],
-        twitterImageHeight: ['300'],
-        ogVideo: ['http://test.com/logo.png'],
-        ogVideoType: ['image/png'],
-        ogVideoWidth: ['300'],
-        ogVideoHeight: ['300'],
-        twitterPlayer: ['http://test.com/logo.png'],
-        twitterPlayerStream: ['image/png'],
-        twitterPlayerWidth: ['300'],
-        twitterPlayerHeight: ['300'],
-        ogDescription: 'stuff',
-      }, {
-        url: 'http://test.com/',
-        allMedia: true,
-      });
-      expect(ogMedia.ogImage).to.eql([{
-        url: 'http://test.com/logo.png',
-        width: '300',
-        height: '300',
-        type: 'image/png',
-      }]);
-      expect(ogMedia.twitterImage).to.eql([{
-        url: 'http://test.com/logo.png',
-        width: '300',
-        height: '300',
-        alt: 'image/png',
-      }]);
-      expect(ogMedia.ogVideo).to.eql([{
-        url: 'http://test.com/logo.png',
-        width: '300',
-        height: '300',
-        type: 'image/png',
-      }]);
-      expect(ogMedia.twitterPlayer).to.eql([{
-        url: 'http://test.com/logo.png',
-        width: '300',
-        height: '300',
-        stream: 'image/png',
-      }]);
-      done();
+
+    expect(ogMedia.twitterImage).to.eql({
+      url: 'http://test.com/logo_two.gif',
+      alt: 'image/gif',
+      width: '300',
+      height: '300',
     });
+
+    done();
+  });
+  it('has no image or video', function (done) {
+    const ogMedia = media.mediaSetup({
+      ogTitle: 'test site',
+      ogType: 'website',
+      ogUrl: 'http://test.com/',
+      ogDescription: 'stuff',
+    }, {});
+
+    expect(ogMedia.ogImage).to.eql(undefined);
+    expect(ogMedia.twitterImage).to.eql(undefined);
+    expect(ogMedia.ogVideo).to.eql(undefined);
+    expect(ogMedia.twitterPlayer).to.eql(undefined);
+
+    done();
+  });
+  it('has video and twitter video', function (done) {
+    const ogMedia = media.mediaSetup({
+      ogVideo: ['http://test.com/logo.png'],
+      ogVideoType: ['image/png'],
+      ogVideoWidth: ['300'],
+      ogVideoHeight: ['300'],
+      twitterPlayer: ['http://test.com/logo.png'],
+      twitterPlayerStream: ['image/png'],
+      twitterPlayerWidth: ['300'],
+      twitterPlayerHeight: ['300'],
+    }, {});
+
+    expect(ogMedia.ogVideo).to.eql({
+      url: 'http://test.com/logo.png',
+      width: '300',
+      height: '300',
+      type: 'image/png',
+    });
+
+    expect(ogMedia.twitterPlayer).to.eql({
+      url: 'http://test.com/logo.png',
+      width: '300',
+      height: '300',
+      stream: 'image/png',
+    });
+
+    done();
+  });
+  it('has music:song', function (done) {
+    const ogMedia = media.mediaSetup({
+      musicSong: ['http://test.com/songurl'],
+      musicSongTrack: ['1'],
+      musicSongDisc: ['1'],
+    }, {});
+
+    expect(ogMedia.musicSong).to.eql({
+      url: 'http://test.com/songurl',
+      track: '1',
+      disc: '1',
+    });
+
+    done();
+  });
+  it('has multiple music:songs with allMedia set to true', function (done) {
+    const ogMedia = media.mediaSetup({
+      musicSong: ['http://test.com/songurl', 'http://test.com/songurl3', 'http://test.com/songurl2', ''],
+      musicSongTrack: ['1', '2', '4', ''],
+      musicSongDisc: ['1', '2', '1', ''],
+    }, {
+      allMedia: true,
+    });
+
+    expect(ogMedia.musicSong).to.eql([{
+      url: 'http://test.com/songurl',
+      track: '1',
+      disc: '1',
+    },
+    {
+      url: 'http://test.com/songurl2',
+      track: '4',
+      disc: '1',
+    },
+    {
+      url: 'http://test.com/songurl3',
+      track: '2',
+      disc: '2',
+    },
+    {
+      url: '',
+      track: '',
+      disc: '',
+    }]);
+
+    done();
+  });
+
+  it('allMedia set to true', function (done) {
+    const ogMedia = media.mediaSetup({
+      ogImage: ['http://test.com/logo.png'],
+      ogImageType: ['image/png'],
+      ogImageWidth: ['300'],
+      ogImageHeight: ['300'],
+      twitterImage: ['http://test.com/logo.png'],
+      twitterImageAlt: ['image/png'],
+      twitterImageWidth: ['300'],
+      twitterImageHeight: ['300'],
+      ogVideo: ['http://test.com/logo.png'],
+      ogVideoType: ['image/png'],
+      ogVideoWidth: ['300'],
+      ogVideoHeight: ['300'],
+      twitterPlayer: ['http://test.com/logo.png'],
+      twitterPlayerStream: ['image/png'],
+      twitterPlayerWidth: ['300'],
+      twitterPlayerHeight: ['300'],
+    }, {
+      allMedia: true,
+    });
+
+    expect(ogMedia.ogImage).to.eql([{
+      url: 'http://test.com/logo.png',
+      width: '300',
+      height: '300',
+      type: 'image/png',
+    }]);
+
+    expect(ogMedia.twitterImage).to.eql([{
+      url: 'http://test.com/logo.png',
+      width: '300',
+      height: '300',
+      alt: 'image/png',
+    }]);
+
+    expect(ogMedia.ogVideo).to.eql([{
+      url: 'http://test.com/logo.png',
+      width: '300',
+      height: '300',
+      type: 'image/png',
+    }]);
+
+    expect(ogMedia.twitterPlayer).to.eql([{
+      url: 'http://test.com/logo.png',
+      width: '300',
+      height: '300',
+      stream: 'image/png',
+    }]);
+
+    done();
   });
 });
