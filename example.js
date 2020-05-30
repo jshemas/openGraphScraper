af166f10be03cab2b03d3693394e15615cfd096e
jshemas <joshshemas@gmail.com>
2020-05-30 19:30:54 -0400

    adding example.js

---
+++ example.js
@@ -0,0 +1,10 @@
+const ogs = require('open-graph-scraper');
+
+const options = { url: 'http://ogp.me/' };
+ogs(options)
+  .then((data) => {
+    const { error, result, response } = data;
+    console.log('error:', error); // This is returns true or false. True if there was a error. The error it self is inside the results object.
+    console.log('result:', result); // This contains all of the Open Graph results
+    console.log('response:', response); // This contains the HTML of page
+  });
