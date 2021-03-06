f4c0d4f1fc61a62020324cc2c8f73a9ee8fadec9
jshemas <joshshemas@gmail.com>
2021-03-05 23:30:10 -0500

    adding notes around proxy test

--- tests/integration/proxy.spec.js
+++ tests/integration/proxy.spec.js
@@ -5,8 +5,9 @@ const ogs = require('../../index');
 describe('proxy', function () {
   it('example of how to use a proxy', function () {
     this.timeout(30000);
+    // first do a normal request to get the local IP
     return ogs({
-      url: 'http://whatismyipaddress.com/',
+      url: 'https://whatismyipaddress.com/',
     }, function (error, result, response) {
       console.log('error:', error);
       console.log('result:', result);
@@ -15,9 +16,11 @@ describe('proxy', function () {
       const $ = cheerio.load(response.body);
       const ipWithoutProxy = $('.address a').attr('href');
       console.log('ip:', ipWithoutProxy);
+      // second do a proxy request and compair the IPs
       return ogs({
-        url: 'http://whatismyipaddress.com/',
+        url: 'https://whatismyipaddress.com/',
         agent: {
+          // proxy agent for https requests
           https: tunnel.httpsOverHttp({
             // proxy was found here: https://hidemy.name/en/proxy-list/?country=US&type=h#list
             proxy: {
@@ -36,6 +39,7 @@ describe('proxy', function () {
         const $$ = cheerio.load(responseWithProxy.body);
         const ipWithProxy = $$('.address a').attr('href');
         console.log('proxyIp:', ipWithProxy);
+        // if proxy agent is working, these IPs should be differnt
         expect(ipWithoutProxy).to.be.not.eql(ipWithProxy);
       });
     });
