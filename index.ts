4919bf7f94c265c8f868f3d2863e6f5b0a8ef92d
jshemas <joshshemas@gmail.com>
2023-02-12 14:33:46 -0500

    adding back in some basic jsdocs

--- index.ts
+++ index.ts
@@ -1,6 +1,31 @@
+/* eslint-disable max-len */
 // eslint-disable-next-line import/no-import-module-exports
 import setOptionsAndReturnOpenGraphResults from './lib/openGraphScraper';
 
+/**
+ * `open-graph-scraper` uses [got](https://github.com/sindresorhus/got) for requests and most of
+ * [got's options](https://github.com/sindresorhus/got/blob/main/documentation/2-options.md)
+ * should work as `open-graph-scraper` options.
+ *
+ * @param {object} options - The options used by Open Graph Scraper
+ * @param {string} options.url - URL of the site. (Required)
+ * @param {string} [options.html] - You can pass in an HTML string to run ogs on it. (use without options.url)
+ * @param {string[]} [options.blacklist] - Pass in an array of sites you don't want ogs to run on.
+ * @param {boolean} [options.onlyGetOpenGraphInfo] - Only fetch open graph info and don't fall back on anything else.
+ * @param {boolean} [options.ogImageFallback] - Fetch other images if no open graph ones are found.
+ * @param {object} [options.customMetaTags] - Here you can define custom meta tags you want to scrape.
+ * @param {boolean} [options.allMedia] - By default, OGS will only send back the first image/video it finds.
+ * @param {number} [options.peekSize] - Sets the peekSize for the request.
+ * @param {number | false} [options.downloadLimit] - Maximum size of the content downloaded from the server, in bytes.
+ * @param {object} [options.urlValidatorSettings] - Sets the options used by validator.js for testing the URL
+ * @param {boolean} [options.decompress] - Set the accept-encoding to `gzip, deflate, br` (default: `true`).
+ * @param {boolean} [options.followRedirect] - Defines if redirect responses should be followed automatically. (default: `true`).
+ * @param {Object<string, string>} [options.headers] - An object containing request headers. Useful for setting the user-agent.
+ * @param {number} [options.maxRedirects] - If exceeded, the request will be aborted and a MaxRedirectsError will be thrown. (default: `10`).
+ * @param {object} [options.retry] - Number of times `og`s will retry the request (default: `2`).
+ * @param {object} [options.timeout] - Timeout of the request.
+ * @returns {Promise} Promise Object with the Open Graph results
+ */
 export default async function run(options: OpenGraphScraperOptions): Promise<ErrorResult | SuccessResult> {
   let results;
   try {
