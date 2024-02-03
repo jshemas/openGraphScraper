db1641e76865dad3401095b008ddf39c07ca1c63
dyoshikawa <yoshikawa.daio@classmethod.jp>
2024-02-03 12:37:33 +0900

    Remove shift-jis.html and update openGraphScraper.spec.ts

--- tests/unit/openGraphScraper.spec.ts
+++ tests/unit/openGraphScraper.spec.ts
@@ -3,9 +3,7 @@ import sinon from 'sinon';
 
 import chardet from 'chardet';
 import { MockAgent, setGlobalDispatcher } from 'undici';
-import path from 'path';
-import fs from 'fs';
-import { decode } from 'iconv-lite';
+import { encode } from 'iconv-lite';
 import ogs from '../../index';
 
 const basicHTML = `
@@ -740,18 +738,32 @@ describe('return ogs', function () {
 
   context('when the character encoding is not UTF-8', function () {
     it('using just a url', function () {
-      const buffer = fs.readFileSync(path.join(__dirname, '../assets', 'shift-jis.html'));
-      const decodedHtml = decode(buffer, 'shift_jis');
+      const html = `
+      <html>
+        <head>
+          <meta charset="shift_jis">
+          <meta property="og:description" content="OG説明">
+          <meta property="og:title" content="OGタイトル">
+          <meta property="foo" content="バー">
+        </head>
+        <body>
+          <h1>こんにちは</h1>
+          <img width="360" src="test.png" alt="テスト画像">
+          <img width="360" alt="テスト画像2">
+        </body>
+      </html>
+      `;
+      const htmlBuffer = encode(html, 'shift_jis');
       mockAgent.get('http://www.test.com')
         .intercept({ path: '/' })
-        .reply(200, buffer);
+        .reply(200, htmlBuffer);
 
       return ogs({ url: 'www.test.com' })
         .then(function (data) {
           expect(data.result.success).to.be.eql(true);
           expect(data.result.ogTitle).to.be.eql('OGタイトル');
           expect(data.result.requestUrl).to.be.eql('http://www.test.com');
-          expect(data.html).to.be.eql(decodedHtml);
+          expect(data.html).to.be.eql(html);
           expect(data.response).to.be.a('response');
         });
     });
