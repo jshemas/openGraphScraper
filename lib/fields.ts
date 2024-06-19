ea309f2cd7bae32c306ccc43088bbc9d03f6305d
jshemas <joshshemas@gmail.com>
2024-06-19 14:55:14 -0400

    adding a Fields type so now fieldName is just a keyof OgObjectInteral

--- lib/fields.ts
+++ lib/fields.ts
@@ -1,10 +1,18 @@
+import type { OgObjectInteral } from './types';
+
+type Fields = {
+  multiple: boolean;
+  property: string;
+  fieldName: keyof OgObjectInteral;
+}[];
+
 /**
  * array of meta tags ogs is looking for
  *
  * @return {array} array of meta tags
  *
  */
-const fields = [
+const fields: Fields = [
   {
     multiple: false,
     property: 'og:title',
