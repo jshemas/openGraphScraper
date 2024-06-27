f84481436dbd5d7fb2760a894040e00d990c8d9f
Manan <manan@neynar.com>
2024-06-27 09:47:51 -0700

    fix: check if script text is non null before parsing

--- lib/extract.ts
+++ lib/extract.ts
@@ -94,7 +94,11 @@ export default function extractMetaTags(body: string, options: OpenGraphScraperO
     $('script').each((index, script) => {
       if (script.attribs.type && script.attribs.type === 'application/ld+json') {
         if (!ogObject.jsonLD) ogObject.jsonLD = [];
-        ogObject.jsonLD.push(JSON.parse($(script).text()));
+        const scriptText = $(script).text();
+        if (scriptText) {
+            ogObject.jsonLD.push(JSON.parse(scriptText));
+        }
+        
       }
     });
   }
