3ad72196e1947cd0d15e7a6ab67bb402da30ce41
jshemas <joshshemas@gmail.com>
2026-06-25 17:39:14 -0700

    fixing singlequotes

--- tests/integration/spotify.spec.ts
+++ tests/integration/spotify.spec.ts
@@ -28,9 +28,9 @@ describe('spotify', function () {
       expect(result.ogLocale).to.be.eql('en');
       expect(result.ogSiteName).to.be.eql('Spotify');
       expect(result.ogImage).to.be.eql([{
-        height: "640",
+        height: '640',
         url: 'https://i.scdn.co/image/ab67616d0000b27397508a4b756763370510bd44',
-        width: "640",
+        width: '640',
       }]);
       expect(result.twitterImage).to.be.eql([{
         url: 'https://i.scdn.co/image/ab67616d0000b27397508a4b756763370510bd44',
@@ -130,9 +130,9 @@ describe('spotify', function () {
       expect(result.ogSiteName).to.be.eql('Spotify');
       expect(result.ogLocale).to.be.eql('en');
       expect(result.ogImage).to.be.eql([{
-        height: "640",
+        height: '640',
         url: 'https://i.scdn.co/image/ab6761610000e5eb6e835a500e791bf9c27a422a',
-        width: "640",
+        width: '640',
       }]);
       expect(result.twitterImage).to.be.eql([{
         url: 'https://i.scdn.co/image/ab6761610000e5eb6e835a500e791bf9c27a422a',
@@ -203,9 +203,9 @@ describe('spotify', function () {
       expect(result.ogSiteName).to.be.eql('Spotify');
       expect(result.ogLocale).to.be.eql('en');
       expect(result.ogImage).to.be.eql([{
-        height: "640",
+        height: '640',
         url: 'https://i.scdn.co/image/ab67616d0000b2730939dadf614e70aeffc6710c',
-        width: "640",
+        width: '640',
       }]);
       expect(result.twitterImage).to.be.eql([{
         url: 'https://i.scdn.co/image/ab67616d0000b2730939dadf614e70aeffc6710c',
