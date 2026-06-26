5a8eb6f45ae4eb0cc05d6837cdf676e6f907eef3
jshemas <joshshemas@gmail.com>
2026-06-25 17:45:43 -0700

    updating comment

--- tests/integration/encoding.spec.ts
+++ tests/integration/encoding.spec.ts
@@ -611,7 +611,7 @@ describe('encoding', function () {
     });
   });
 
-  // this test is skipped because it doesn't work in CI/CD environment, but works locally. It is a test for ShiftJIS encoding.
+  // this test is skipped because it doesn't work in CI/CD environment, but works locally.
   it.skip('abehiroshi - ShiftJIS', function () {
     return ogs({ url: 'http://abehiroshi.la.coocan.jp/' })
       .then(function (data) {
